b656a68b95330648c542adfd757b7372fb8839b7
Kaniesa <kaniesadeswal@gmail.com>
2025-06-24 11:52:05 -0500

    Updated listen_pos_estimate to set closed loop control on motor for pos estimates

--- odrive_scripts/can_code/listen_pos_estimates.py
+++ odrive_scripts/can_code/listen_pos_estimates.py
@@ -10,15 +10,17 @@ from can_joint import CanJoint #get CanJoint node class
 
 # setup
 can_bus = can.Bus("can0", bustype="socketcan")
-joint = CanJoint(can_bus=can_bus, can_node_id=0, gear_ratio=8, offset=0)
+joint = CanJoint(can_bus=can_bus, can_node_id=1, gear_ratio=8, offset=0)
 time.sleep(0.01)
+joint.arm_closed_loop() # Pos Estimate prints zero if motors are in IDLE mode
+# joint.disarm()
 
 # cycle through each message type from all 12 motors in loop
 for msg in can_bus:
     for i in range(0,12):
         # check if message ID matches encoder message (0x009) from motor i
         if msg.arbitration_id == (0x009 | i << 5):
-            AxisPosEstimate = 'Axis' + str(i) + '_Get_Encoder_Estimates'
+            AxisPosEstimate = 'Axis' + str(1) + '_Get_Encoder_Estimates'
             ID_number = i
             print("\nReceived Axis", ID_number, " encoder message:")
             # create and print Encoder message
